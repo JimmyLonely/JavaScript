a333a4da186a5c64ab359eb662515919dfc626c4
Jimmy.Li <Jimmy.Li@msn.com>
2017-02-28 14:56:30 +0800

    Update JavaScript.pdf V3.7.

--- js.js
+++ js.js
@@ -1,3 +1,15 @@
 function sayHello() {
-    alert('<\/script>');
+    var i = 12;
+
+    switch (i) {
+        case 1:
+            console.log(1);
+            break;
+        case "12":
+            console.log(12);
+        default:
+            console.log('Default');
+    }
 }
+
+sayHello();
