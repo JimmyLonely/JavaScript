d6b5671793ef8e22170dfaf5ba2671a511ba0811
Jimmy.Li <Jimmy.Li@msn.com>
2017-02-28 15:29:35 +0800

    Update JavaScript.pdf V4.0.

--- js.js
+++ js.js
@@ -12,4 +12,20 @@ function sayHello() {
     }
 }
 
-sayHello();
+function doAdd() {
+    'use strict'
+    switch (arguments.length) {
+        case 1:
+            console.log(arguments[0]);
+            break;
+        case 2:
+            console.log(arguments[0] + arguments[1]);
+            break;
+        case 3:
+            console.log(arguments[0] + arguments[1] + arguments[2]);
+            break;
+        case 4:
+            console.log(arguments[0] + arguments[1] + arguments[2] + arguments[3]);
+            break;
+    }
+}
