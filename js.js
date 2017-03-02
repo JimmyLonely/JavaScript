5248a2e45f5813aa86cc2efddb7d0b8baec2bb46
Jimmy.Li <Jimmy.Li@msn.com>
2017-03-02 10:30:06 +0800

    The scope of variable.

--- js.js
+++ js.js
@@ -1,31 +1,8 @@
-function sayHello() {
-    var i = 12;
-
-    switch (i) {
-        case 1:
-            console.log(1);
-            break;
-        case "12":
-            console.log(12);
-        default:
-            console.log('Default');
-    }
-}
-
-function doAdd() {
+function addValue(num1, num2){
     'use strict'
-    switch (arguments.length) {
-        case 1:
-            console.log(arguments[0]);
-            break;
-        case 2:
-            console.log(arguments[0] + arguments[1]);
-            break;
-        case 3:
-            console.log(arguments[0] + arguments[1] + arguments[2]);
-            break;
-        case 4:
-            console.log(arguments[0] + arguments[1] + arguments[2] + arguments[3]);
-            break;
-    }
+    sum = num1 + num2;
+    return sum;
 }
+
+addValue(10, 20);
+console.log(sum);
