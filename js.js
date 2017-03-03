056ba4ad5519d479bd7153aacec74349bac8cf48
Jimmy.Li <Jimmy.Li@msn.com>
2017-03-03 10:39:08 +0800

    Array system function.

--- js.js
+++ js.js
@@ -1,5 +1,7 @@
-var numArray = [0, 1, 2, 5, 6, 8, 9, 12, 21];
+var me = [1, 2, 3, 4, 5, 6];
 
-function compare(value1, value2) {
-    return value1 - value2;    
-}
+var sum = me.reduce(function (pre, cur, index, arry) {
+    return pre + cur;
+}, 21);
+
+console.log(sum);
