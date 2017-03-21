5f2aa00cc3054ff566a763f584271b433a7095ac
Jimmy.Li <Jimmy.Li@msn.com>
2017-03-21 17:21:09 +0800

    function matchesSelector.

--- js.js
+++ js.js
@@ -1,1 +1,18 @@
-console.log('js.js');
+
+function matchesSelector(element, selector) {
+    if (element.matchesSelector) {
+        return element.matchesSelector(selector);
+    } else if (element.webkitMatchesSelector) {
+        return element.webkitMatchesSelector(selector);
+    } else if (element.msMatchesSelector) {
+        return element.msMatchesSelector(selector);
+    }
+    else if (element.mozMatchesSelector) {
+        return element.mozMatchesSelector(selector);
+    }
+    else {
+        throw new Error('Not supported.');
+    }
+}
+
+console.log(matchesSelector(document.body, 'body'));
