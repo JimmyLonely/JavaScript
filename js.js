bfe7b843f893d375903c93b744eedd6b4278d080
Jimmy.Li <Jimmy.Li@msn.com>
2017-04-05 13:27:44 +0800

    event contextmenu and beforeunload.

--- js.js
+++ js.js
@@ -1,36 +1,18 @@
 
+var div = document.querySelector('#myDiv');
+var myMenu = document.querySelector('#myMenu');
 
-var btn = document.querySelector('button');
-
-window.addEventListener('load', function (event) {
-    var image = document.createElement('img');
-
-    document.body.appendChild(image);
-    image.src = '/asset/img/png.png';
-    image.addEventListener('load', function (event) {
-        console.log(event.target);
-    }, false);
+window.addEventListener('contextmenu', function (event) {
+    event.preventDefault();
+    myMenu.style.visibility = 'visible';
 }, false);
 
-window.addEventListener('load', function (event) {
-    var js = document.createElement('script');
-    js.addEventListener('load', function (event) {
-        console.log(event.target);
-    }, false);
-
-    document.body.appendChild(js);
-    js.src = '/asset/js/example.js';
+document.addEventListener('click', function () {
+    myMenu.style.visibility = 'hidden';
 }, false);
 
-window.addEventListener('load', function (event) {
-    var link = document.createElement('link');
-    link.type = "text/css";
-    link.rel = "stylesheet";
-
-    link.addEventListener('load', function (event) {
-        console.log(event.target);
-    }, false);
-
-    document.querySelector('head').appendChild(link);
-    link.href = '/asset/css/example.css';
+window.addEventListener('beforeunload', function (event) {
+    var message = 'Are you sure to leave the website ?'
+    event.returnValue = message;
+    return message;
 }, false);
