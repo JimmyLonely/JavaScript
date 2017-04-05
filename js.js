cb87bcb8c79c2421c2a6edda61a72650b8fd2a46
Jimmy.Li <Jimmy.Li@msn.com>
2017-04-05 15:25:01 +0800

    simulate event with custom event.

--- js.js
+++ js.js
@@ -1,18 +1,11 @@
 
-var div = document.querySelector('#myDiv');
-var myMenu = document.querySelector('#myMenu');
+var div = document.getElementById('div');
+var btn = document.getElementById('button');
+var text = document.getElementById('text');
 
-window.addEventListener('contextmenu', function (event) {
-    event.preventDefault();
-    myMenu.style.visibility = 'visible';
-}, false);
+eventUtil.addHandler(btn, 'custom', function (event) {
+    console.log('custom.');
+})
 
-document.addEventListener('click', function () {
-    myMenu.style.visibility = 'hidden';
-}, false);
 
-window.addEventListener('beforeunload', function (event) {
-    var message = 'Are you sure to leave the website ?'
-    event.returnValue = message;
-    return message;
-}, false);
+//var event = document.createEvent('MouseEvent');
